aa90a480271fd9264b8ebcbd797791589cd6b345
Andreas Wälchli <andreas.waelchli@me.com>
2017-10-02 14:58:45 +0200

    no idea

---
+++ src/main/scala/ch/awae/imgtagger/view/App.scala
@@ -0,0 +1,16 @@
+package ch.awae.imgtagger.view
+
+import javafx.application.Application
+import javafx.stage.Stage
+
+class App extends Application {
+
+  def start(x$1: Stage): Unit = {
+
+  }
+
+  override def stop(): Unit = {
+
+  }
+
+}
